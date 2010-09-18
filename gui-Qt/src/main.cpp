bb245d9f36bfb38fc13d5effea13f257922f3bfd
Joerg-C. Boehme <joerg@chaosdorf.de>
2010-09-18 12:45:43 +0200

    Connection Dialog eingebaut.

--- gui-Qt/src/main.cpp
+++ gui-Qt/src/main.cpp
@@ -1,5 +1,7 @@
 #include <QtGui>
+#include <QtSql>
 
+#include "ConnectionDialog.h"
 #include "MemberDialog.h"
 #include "MemberModel.h"
 #include "MainWindow.h"
@@ -8,17 +10,28 @@ int main(int argc, char *argv[])
 {
 	QApplication app(argc, argv);
 
-	// XXX nur zum Test!
-	QSqlDatabase db = QSqlDatabase::addDatabase("QSQLITE");
-	db.setDatabaseName("dorfverw.dat");
-	db.open();
+	ClubFrontend::ConnectionDialog dialog;
+	if(dialog.exec() != QDialog::Accepted) {
+		return 0;
+	}
 
-	ClubFrontend::MemberModel dataSource(QSqlDatabase::database());
+	QSqlDatabase db = QSqlDatabase::addDatabase(dialog.getDriver());
+	db.setDatabaseName(dialog.getDatabaseName());
+	db.setPort(dialog.getPort());
+	db.setPassword(dialog.getPassword());
+	db.setUserName(dialog.getUsername());
+	if(!db.open()) {
+		QSqlError err = db.lastError();
+		if(err.type() != QSqlError::NoError) {
+			QMessageBox::warning(0, QObject::tr("Unable to open database"),
+					QObject::tr("An error occured while opening the connection: ") + err.text());
+			return 1;
+		}
+	}
+
+	ClubFrontend::MemberModel dataSource(db);
 	ClubFrontend::MainWindow* mainWindow = new ClubFrontend::MainWindow(dataSource);
 	mainWindow->show();
-
-//    GuiVerwaltung::MemberDialog *dialog = new GuiVerwaltung::MemberDialog;
-//    dialog->show();
 
 	return app.exec();
 }
