b7454ad852ed1ccc5a515a9ad6b49848106e6f95
Joerg-C. Boehme <joerg@chaosdorf.de>
2010-11-06 11:43:44 +0100

    Spalten ausgeblendet.

--- gui-Qt/src/SaldoDialog.cpp
+++ gui-Qt/src/SaldoDialog.cpp
@@ -10,7 +10,9 @@ SaldoDialog::SaldoDialog(SaldoModel& aSaldoModel, QWidget *parent)
     ui.setupUi(this);
     ui.saldoTableView->setModel(saldoModel.getSaldoTableModel());
     ui.saldoTableView->sortByColumn(SaldoTable::saldo_pkey, Qt::DescendingOrder);
-
+    
+    ui.saldoTableView->setColumnHidden(SaldoTable::saldo_pkey, true);
+    ui.saldoTableView->setColumnHidden(SaldoTable::dorfmitglied_pkey, true);
 }
 
 SaldoDialog::~SaldoDialog()
