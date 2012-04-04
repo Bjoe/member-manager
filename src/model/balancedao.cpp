fd92462cea51155a6910eb5a777900b0f094ad05
Joerg-Christian Boehme <joerg@chaosdorf.de>
2012-04-04 20:52:33 +0200

    Delete some comments.

--- src/model/balancedao.cpp
+++ src/model/balancedao.cpp
@@ -1,7 +1,6 @@
 #include "balancedao.h"
 
 #include "model/databasestructure.h"
-#include "model/memberfilter.h"
 
 namespace membermanager {
 namespace model {
@@ -13,12 +12,9 @@ BalanceDao::BalanceDao(const QSqlDatabase &aDatabase) : database(aDatabase)
 bool BalanceDao::saveRecord(const accounting::BalanceEntry &anEntry)
 {
     QSqlRecord record = anEntry.balanceRecord;
-//    int memberId = record.value(SaldoTable::dorfmitglied_pkey).toInt();
 
     QSqlTableModel model(0, database);
     model.setTable(SaldoTable::TABLENAME);
-//    model.setFilter(MemberFilter::build().withMemberId(memberId).createFilter());
-//    model.select();
 
     bool successful = model.insertRecord(-1, record);
     printSqlError(model.lastError());
