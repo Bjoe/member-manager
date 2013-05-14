174eb6f522668d6474864bc76765beb5e6416da9
Joerg-Christian Boehme <joerg@chaosdorf.de>
2013-05-14 22:48:16 +0200

    Change output from html to Latex txt in contributionreceiptdialog.

--- src/gui/contributionreceiptdialog.cpp
+++ src/gui/contributionreceiptdialog.cpp
@@ -42,34 +42,38 @@ void ContributionReceiptDialog::showContribution()
 
 
     QString address;
-    address.append(QString("%1 ").arg(member.getFirstname())).append(QString("%1<br>").arg(member.getName()));
-    address.append(QString("%1<br>").arg(member.getStreet()));
-    address.append(QString("%1 ").arg(member.getZipCode())).append(QString("%1<br>").arg(member.getCity()));
+    address.append(QString("%1 ").arg(member.getFirstname())).append(QString("%1 \\\\ \n").arg(member.getName()));
+    address.append(QString("%1 \\\\ \n").arg(member.getStreet()));
+    address.append(QString("%1 ").arg(member.getZipCode())).append(QString("%1 \n\n").arg(member.getCity()));
+    address.append(QString("%1 %2, %3, %4 %5 \n\n")
+            .arg(member.getFirstname())
+            .arg(member.getName())
+            .arg(member.getStreet())
+            .arg(member.getZipCode())
+            .arg(member.getCity()));
 
     QString timeRange = QString(tr("vom 01.01.%1 bis zum 31.12.%1")).arg(year);
 
-    QString contribution = QString("<table>");
+    QString contribution("\n");
     double sum = 0;
     accounting::BalanceEntry balance;
     foreach(balance, balanceList) {
-        contribution.append("<tr>");
         QDate valuta = balance.getValuta();
         double value = balance.getValue();
-        contribution.append(QString("<td>%1 /</td>").arg(valuta.toString("dd.MM.yyyy")));
-        contribution.append(QString("<td align=\"right\">%L1 Euro / </td>").arg(value, 7, 'f', 2, ' '));
-        contribution.append(QString("<td>%1</td>").arg(balance.getPurpose()));
-        contribution.append("</tr>");
+        contribution.append(QString("%1 & ").arg(valuta.toString("dd.MM.yyyy")));
+        contribution.append(QString("%L1 Euro & ").arg(value, 7, 'f', 2, ' '));
+        contribution.append(QString("%1 \\\\ \n").arg(balance.getPurpose()));
         sum += value;
     }
-    contribution.append("</table>");
-    contribution.append(QString("<br>Summe: %L1 Euro").arg(sum, 7, 'f', 2));
+    contribution.append("\n");
+    contribution.append(QString("Summe: %L1 Euro\n").arg(sum, 7, 'f', 2));
 
     QString content;
-    content.append(QString("%1<br>").arg(address));
-    content.append(QString("%1<br><br>").arg(timeRange));
-    content.append(QString("%1<br>").arg(contribution));
+    content.append(QString("%1\n").arg(address));
+    content.append(QString("%1\n").arg(timeRange));
+    content.append(QString("%1\n").arg(contribution));
 
-    ui.textEdit->setHtml(content);
+    ui.textEdit->setText(content);
 }
 
 } // namespace gui
