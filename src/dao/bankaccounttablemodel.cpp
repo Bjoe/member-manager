9b947aa7c2d83e00f331cfe43a447e50ba55564b
Joerg-Christian Boehme <joerg@chaosdorf.de>
2013-09-09 00:26:13 +0200

    Remove waste debug output.

--- src/dao/bankaccounttablemodel.cpp
+++ src/dao/bankaccounttablemodel.cpp
@@ -4,8 +4,6 @@
 #include "QDjangoQuerySet.h"
 #include "QDjangoWhere.h"
 
-#include <QDebug>
-
 namespace membermanager {
 namespace dao {
 
@@ -13,8 +11,6 @@ entity::BankAccount *BankAccountTableModel::findByMemberId(QVariant id)
 {
     QDjangoQuerySet<entity::BankAccount> querySet;
     QDjangoQuerySet<entity::BankAccount> result = querySet.filter(QDjangoWhere("memberId", QDjangoWhere::Equals, id));
-
-    qDebug() << "count" << result.count();
 
     entity::BankAccount *bankAccount = new entity::BankAccount();
     result.at(0, bankAccount);
